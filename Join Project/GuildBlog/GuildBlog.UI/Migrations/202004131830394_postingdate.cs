5ee5075c8070a84cda2534aa48030c6fe1817711
patmcannon <patmcannon@gmail.com>
2020-04-13 13:32:44 -0500

    Added posting date

---
+++ Join Project/GuildBlog/GuildBlog.UI/Migrations/202004131830394_postingdate.cs
@@ -0,0 +1,18 @@
+﻿namespace GuildBlog.UI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class postingdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Postings", "PostingDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Postings", "PostingDate");
+        }
+    }
+}
--- Join Project/GuildBlog/GuildBlog.UI/Models/Posting.cs
+++ Join Project/GuildBlog/GuildBlog.UI/Models/Posting.cs
@@ -8,6 +8,7 @@ namespace GuildBlog.UI.Models
     public class Posting
     {
         public int PostingID { get; set; }
+        public DateTime PostingDate { get; set; }
         public string PostingTitle { get; set; }
         public string PostingAuthor { get; set; }
         public string PostingBody { get; set; }
