eaf325efb658ace0eb099fbf30199e9d16e44361
patmcannon <patmcannon@gmail.com>
2020-04-13 10:23:34 -0500

    Implement code first EF first stage.
    Created the DbContext but not the models.

---
+++ Join Project/GuildBlog/GuildBlog.UI/Models/EF/GuildBlogEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GuildBlog.UI.Models.EF
+{
+    public class GuildBlogEntities : DbContext
+    {
+        public GuildBlogEntities() 
+            :base("GuildBlog")
+        {
+
+        }
+    }
+}
