f09fd367984151c82b3777a65d725e977798ac85
patmcannon <patmcannon@gmail.com>
2020-04-13 21:31:46 -0500

    Seed data added

--- Join Project/GuildBlog/GuildBlog.UI/Migrations/Configuration.cs
+++ Join Project/GuildBlog/GuildBlog.UI/Migrations/Configuration.cs
@@ -22,11 +22,10 @@
             //  to avoid creating duplicate seed data.
 
             context.Roles.AddOrUpdate(
-    r => r.Name,
-    new IdentityRole { Name = "Owner" },
-    new IdentityRole { Name = "Admin" }
-    );
-
+                r => r.Name,
+                new IdentityRole { Name = "Owner" },
+                new IdentityRole { Name = "Admin" }
+                );
             context.Users.AddOrUpdate(
                 new ApplicationUser
                 {
@@ -48,8 +47,7 @@
                     PostingAuthor = "John Doe",
                     PostingBody = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     PostingDate = DateTime.Today
-                }
-                );
+                });
         }
     }
 }
