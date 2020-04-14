ee0bb7039e5c032236f89789bf3ae61f9076bb1d
DavidTsolak <david.tsolak@gmail.com>
2020-04-14 09:54:07 -0500

    I added some spaces

--- Joint/GuildBlog/GuildBlog.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuildBlog.UI.Global" Language="C#" %>
--- Joint/New folder/createDatabase.sql
+++
@@ -1,9 +0,0 @@
-USE master
-GO
-
-IF EXISTS(SELECT * FROM sys.databases WHERE name='GuildBlog')
-DROP DATABASE GuildBlog
-GO
-
-CREATE DATABASE GuildBlog
-GO
